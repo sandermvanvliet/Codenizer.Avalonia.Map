7e1caf0027c081d098fc3993cdef9ce4990e2559
Sander van Vliet <sander@codenizer.nl>
2024-09-03 15:20:01 +0200

    Update copyright notice

--- apply-license.ps1
+++ apply-license.ps1
@@ -8,11 +8,11 @@ $prepend = Get-Content "csharp-license.txt" -Raw
 
 foreach($file in $csharpFiles)
 {
-    $contents = get-content $file -head 1
-    if($contents.Trim() -ne "// Copyright (c) 2023 Sander van Vliet")
+    $fullPath = $file.FullName
+    $contents = get-content $fullPath -head 1
+    
+    if(!($contents.Trim().StartsWith("// Copyright (c)")))
     {
-        $fullPath = $file.FullName
-
         Write-Host "$fullPath doesn't have a license header"
 
         $rawContents = Get-Content $file -Raw
